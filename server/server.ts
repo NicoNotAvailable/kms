6e4473cf0fd71ec6537c41d20e7630e567282b6f
Nico Hofmann <nico.hofmann@mni.thm.de>
2024-04-22 16:03:09 +0200

    delete todo

--- server/server.ts
+++ server/server.ts
@@ -7,15 +7,19 @@ app.get("/", sendMainpage);
 
 app.post("/todo")
 app.get("/todo")
-app.delete("/todo/:id")
+app.delete("/todo/:id", deleteTodo);
+app.patch("/todo/:id", changeTodo);
 
 export class ToDoEntry {
+    id: number;
     title: string;
     description: string;
     status: boolean;
     priority: number;
 
     constructor(title: string, description: string, priority: number) {
+        this.id = incrementedId;
+        incrementedId++;
         this.title = title;
         this.description = description;
         this.priority = priority;
@@ -23,10 +27,33 @@ export class ToDoEntry {
     }
 }
 
+let incrementedId: number = 0;
+
 let todoList: ToDoEntry[] = [];
 
 
 function sendMainpage(req: express.Request, res: express.Response) {
     res.status(200);
     res.sendFile(`${__dirname}/client/index.html`);
+}
+
+function deleteTodo(req: express.Request, res: express.Response): void {
+    let todoIndex: number = Number(req.params.id);
+    let deletedEntry: ToDoEntry[];
+    for (let i: number = 0; i < todoList.length; i++) {
+        if (todoList[i].id === todoIndex) {
+            deletedEntry = todoList.splice(i, 1);
+            break;
+        }
+    }
+    if (deletedEntry !== null) {
+        res.status(200);
+    } else{
+        res.status(404);
+        res.json({msg: 'Todo Entry not found!'});
+    }
+}
+
+function changeTodo(req: express.Request, res: express.Response): void {
+
 }
