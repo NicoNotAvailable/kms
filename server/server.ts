49781547b332aa76f15a8e5556aaea79303656b2
TinaHaser <tina.haser@mni.thm.de>
2024-04-22 15:46:50 +0200

    class

--- server/server.ts
+++ server/server.ts
@@ -4,9 +4,22 @@ const app: express.Express = express();
 app.listen(8080);
 
 
-
 app.post("/todo")
 app.get("/todo")
 app.delete("/todo/:id")
 
+export class ToDoEntry {
+    title: string;
+    description: string;
+    status: boolean;
+    priority: number;
 
+    constructor(title: string, description: string, priority: number) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = false;
+    }
+
+
+}
